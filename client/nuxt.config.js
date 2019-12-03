321ba2bc90c65373e58c8ce95c2b1936a718d28e
rye gaspar <gaspar.ryan@outlook.com>
2019-12-02 20:23:29 -0600

    004 - base styles with tailwind

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -11,7 +11,8 @@ export default {
 			{hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
 		],
 		link: [
-			{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+			{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+			{rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rubik:400,500&display=swap'}
 		]
 	},
 	/*
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="container">
-		Home
+		<h1 class="font-medium text-4xl">Snippets</h1>
+		<p>test <a href="#">Click here</a></p>
+		<p>Lorem Ipsum dolor sit met</p>
 	</div>
 </template>
 
