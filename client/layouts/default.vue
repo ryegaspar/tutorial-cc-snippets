a3c12c680b6a03cde49e1372bbfc8b5877c0fe39
rye gaspar <gaspar.ryan@outlook.com>
2019-12-02 20:52:04 -0600

    005 - designing the navigation

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
 	<div>
+		<the-navigation></the-navigation>
 		<nuxt/>
 	</div>
 </template>
+
+<script>
+	import TheNavigation from "@/components/global/TheNavigation";
+
+	export default {
+		components: {
+			TheNavigation
+		}
+	}
+</script>
