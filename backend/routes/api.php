5181ed6d0410f7aa7754544ff56459a04189dac2
rye gaspar <gaspar.ryan@outlook.com>
2019-12-03 19:29:17 -0600

    011 - signing out

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,4 +3,5 @@
 Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
     Route::post('signin', 'SignInController');
     Route::get('me', 'MeController');
+    Route::post('signout', 'SignOutController');
 });
