11cbaa1b8a342d19dfb88d7c5f8b9b7f60849899
rye gaspar <gaspar.ryan@outlook.com>
2019-12-17 18:38:55 -0600

    041 - syntax highlighting

--- client/components/snippets/StepMarkdown.vue
+++ client/components/snippets/StepMarkdown.vue
@@ -4,7 +4,19 @@
 </template>
 
 <script>
-	let markdown = require('markdown-it')();
+	import hljs from 'highlight.js';
+
+	let markdown = require('markdown-it')({
+			highlight(str, lang) {
+				let esc = markdown.utils.escapeHtml;
+
+				if (lang && hljs.getLanguage(lang)) {
+					return '<pre class="hljs language-' + esc(lang.toLowerCase()) + '"><code>' + hljs.highlightAuto(esc(str)).value + '</code></pre>';
+				}
+				return '<pre class="hljs"><code>' + esc(str) + '</code></pre>';
+			}
+		})
+	;
 
 	export default {
 		props: {
--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -25,7 +25,7 @@ export default {
 	** Global CSS
 	*/
 	css: [
-		'~/assets/styles/app.css'
+		'~/assets/styles/app.scss'
 	],
 	/*
 	** Plugins to load before mounting the App
